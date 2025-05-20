f60e7580330482705dbc65b6c03c4c66517d8af0
BD-Arfat <arfat1islam5162@gmail.com>
2025-05-20 00:18:40 -0700

    first commit

---
+++ variable.js
@@ -0,0 +1,17 @@
+// variable
+
+const weight = 74;
+const price = 20000;
+const year = 2025;
+const balance = 500;
+const age = 20;
+
+// problem for practice
+
+const dateOfBarth = 2005;
+const brotherSister = 2;
+const student = 52;
+const marks = 50;
+const rooms = 7;
+const dutyTime = 8;
+const total = 11;
