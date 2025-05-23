1d0e3e55895c65a08015009b756a9bf3d128fa43
BD-Arfat <arfat1islam5162@gmail.com>
2025-05-22 22:25:20 -0700

    console, string

---
+++ console.js
@@ -0,0 +1,19 @@
+// console
+
+// console.log('name');
+// console.log(534);
+// console.log(false);
+
+// problem for practice
+
+// const age = 55;
+// console.log(age);
+
+// const bookToRead = 5;
+// console.log(bookToRead);
+
+// const wakeToTime = 8;
+// console.log(wakeToTime);
+
+// const wastedTime = 4;
+// console.log(wastedTime)
---
+++ string.js
@@ -0,0 +1,24 @@
+// string
+
+// const name = "sadaruddin";
+// const address = 'kumira, shitakhuda, chittagong';
+// const favFood = 'Biryani';
+// const drinking = "Tea";
+// const subject = "English";
+// const teacher = "Dragon Delwar";
+// const phone = "Samsung";
+// const fruit = "watermelon";
+
+// problem for practice
+
+const name = 'ariful islam arfat';
+console.log(name);
+
+const address = 'kazipara, alom miyar bari';
+console.log(address)
+
+const myCollageName = 'lotifa siddik collage';
+console.log(myCollageName);
+
+const hobby = 'front end developer bangladesh remote';
+console.log(hobby);
