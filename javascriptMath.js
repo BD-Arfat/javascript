2a9fd74d00f41b5c88eb32f4389135670297d1c9
BD-Arfat <arfat1islam5162@gmail.com>
2025-05-23 18:05:35 -0700

    javascriptMath and trueAndFalse

---
+++ javascriptMath.js
@@ -0,0 +1,44 @@
+// javascript math 
+
+// const eggPrice = 10;
+// const onionprice = 20;
+
+// const total = eggPrice + onionprice;
+// console.log(total);
+
+// const result = onionprice - eggPrice;
+// console.log(result);
+
+// const gunRestlt = onionprice * eggPrice;
+// console.log(gunRestlt)
+
+// const vagResult = onionprice / eggPrice;
+// console.log(vagResult);
+
+// const parsentResult = onionprice % eggPrice;
+// console.log(parsentResult);
+
+// const fistName = 'arfat ';
+// const lastName = 'islam';
+// const fullName = fistName + lastName;
+// console.log(fullName);
+
+// **** problem for practice ***
+
+const income = 100;
+const totalKoroch = 40;
+
+const bakitaka = income - totalKoroch;
+// console.log(bakitaka)
+
+const pancil = 10;
+const koroch = 2;
+
+const use = pancil / koroch;
+// console.log(use)
+
+const chal = 153;
+const dal = 261;
+
+const totalCost = chal * dal;
+console.log(totalCost)
---
+++ trueAndFalse.js
@@ -0,0 +1,19 @@
+// true or false
+
+const moneyMove = true;
+const learning = true;
+const isChilling = true;
+const isHomeworkDone = false;
+const isCrushReplied = false;
+const hasPhoneBattery = false;
+const singleForever = true;
+const passed = false;
+
+const isRich = true;
+const passeds = false;
+console.log(isRich);
+console.log(passed);
+
+// problem for practice
+
+const friendExam = false;
