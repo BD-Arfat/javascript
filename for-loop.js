45f839dbfa72ac0cf0131cfa5da69acab2827619
BD-Arfat <arfat1islam5162@gmail.com>
2025-05-31 23:28:54 -0700

    for loop add

---
+++ for-loop.js
@@ -0,0 +1,14 @@
+for(let num = 0; num <20; num++){
+    console.log(num)
+}
+let sum = 0;
+for(let i = 20; i <50; i++){
+    sum = sum + i;
+};
+console.log(sum);
+
+for(let i = 0; i < 20; i++){
+    if(i % 2 == 1){
+        console.log(i)
+    }
+}
--- loop.js
+++ loop.js
@@ -20,7 +20,27 @@ while (nums < 5) {
 }
 
 let name = 0;
-while(name < 5){
+while (name < 5) {
     name++;
     console.log('amar name ariful islam arfat')
 }
+
+let numbers2 = 1;
+let sum = 0;
+
+while (numbers2 <= 10) {
+    console.log(numbers2)
+    sum = sum + numbers2;
+    numbers2++;
+}
+console.log(sum)
+
+let numbers3 = 50;
+let sum2 = 0;
+
+while(numbers3 <= 100){
+    // console.log(numbers3)
+    sum2 = sum2 + numbers3
+    numbers3++
+}
+console.log(sum2)
